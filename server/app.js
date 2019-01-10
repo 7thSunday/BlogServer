c3aecf0f4a39b5275afb3e121b7bf373f4dc9756
7thSunday <1064578256@qq.com>
2019-01-10 10:02:06 +0800

    upload test

--- server/app.js
+++ server/app.js
@@ -1,36 +1,49 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const controller = require('./controller')
-const cors = require('koa2-cors')
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const controller = require("./controller");
+const cors = require("koa2-cors");
+const koaBody = require("koa-body");
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
 app.use(cors());
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(
+  koaBody({
+    multipart: true,
+    formidable: {
+      maxFileSize: 20000 * 1024 * 1024 // 设置上传文件大小最大限制，默认2M
+    }
+  })
+);
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"]
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public"));
 
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
+app.use(
+  views(__dirname + "/views", {
+    extension: "ejs"
+  })
+);
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // controller
 app.use(controller());
@@ -38,8 +51,8 @@ app.use(controller());
 app.listen(2333);
 
 // error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
 });
 
-module.exports = app
+module.exports = app;
--- server/controllers/article.js
+++ server/controllers/article.js
@@ -1,35 +1,71 @@
-var fn_test = async (ctx, next) => {
-    let name = ctx.query.name,
-        age = ctx.query.age,
-        gender = ctx.query.gender;
-    ctx.response.body = {
-        'name': name,
-        'age': age,
-        'gender': gender
-    }
-    // await ctx.render('article',{name,age,gender});
-}
+const path = require("path");
+const fs = require("fs");
 
-var my_test = async (ctx, next) => {
-    let testModel = require('../models/test');
-    let timeStamp = Date.now();
-    let test = await testModel.create({
-        id: 'd-' + timeStamp,
-        name: 'Aya',
-        gender: false,
-        birth: '2011-11-23',
-        createAt: timeStamp,
-        updateAt: timeStamp,
-        version: 0
-    });
-    console.log('create' + JSON.stringify(test));
-    ctx.response.body = {
-        'code' : 200,
-        'message' : 'OK'
+// normal request
+var fn_test = async (ctx, next) => {
+  let name = ctx.query.name,
+    age = ctx.query.age,
+    gender = ctx.query.gender;
+  ctx.response.body = {
+    name: name,
+    age: age,
+    gender: gender
+  };
+  // await ctx.render('article',{name,age,gender});
+};
+
+// sql database operation
+var sql_test = async (ctx, next) => {
+  let testModel = require("../models/test");
+  let timeStamp = Date.now();
+  let test = await testModel.create({
+    id: "d-" + timeStamp,
+    name: "Aya",
+    gender: false,
+    birth: "2011-11-23",
+    createAt: timeStamp,
+    updateAt: timeStamp,
+    version: 0
+  });
+  console.log("create" + JSON.stringify(test));
+  ctx.response.body = {
+    code: 200,
+    message: "OK"
+  };
+};
+
+// file upload
+var upload_test = async (ctx, next) => {
+  const tmpdir = path.join("./", "upload_files");
+
+  const filePaths = [];
+  const files = ctx.request.files || {};
+  const params = ctx.request.body.fields;
+  //   console.log(params);
+  for (let key in files) {
+    const file = files[key];
+    if (Object.prototype.toString.call(file) == "[object Array]") {
+      for (var j = 0; j < file.length; j++) {
+        witeFile(file[j]);
+      }
+    } else {
+      witeFile(file);
     }
-} 
+  }
+
+  function witeFile(file) {
+    const filePath = path.join(tmpdir, file.name);
+    const reader = fs.createReadStream(file.path);
+    const writer = fs.createWriteStream(filePath);
+    reader.pipe(writer);
+    filePaths.push(filePath);
+  }
+
+  ctx.body = filePaths;
+};
 
 module.exports = {
-    'GET /test': fn_test,
-    // 'GET /mytest': my_test
-}
+  "GET /test": fn_test,
+  // 'GET /sqltest': sql_test,
+  "POST /upload": upload_test
+};
